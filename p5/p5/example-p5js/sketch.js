c69a6a51356973f48e0cf1f6da3ad3cd6ebe67bf
yamil esquivel <niisanyamil@gmail.com>
2020-07-14 22:21:36 -0300

    features de pagina de touchgui p5js

--- p5/p5/example-p5js/sketch.js
+++ p5/p5/example-p5js/sketch.js
@@ -1,52 +1,62 @@
-//accion de camara
-//var capvid;
 
-//interfaz de usuario
-let gui;
-let on;
-let t;
+var capvid; //camara
+let gui;    //interfaz grafica de usuario
+let on;     //boton 
+let t;       //toogle
+let i=0;     //contador
+let tx ;     //texto
 
 function setup() {
   // put setup code here
-  createCanvas(windowWidth,windowHeight);
-
-  ///pixelDensiti
-  /*
+  createCanvas(windowWidth,windowHeight);  
   capvid =createCapture(VIDEO);
   capvid.size(320,240);
   capvid.hide();
   
-
-
-   // Create Toggle
-  t = createToggle("Toggle", 100, 50, 200, 50);   */
-   //cargando un boton
+  cursor("text("letter",mouseX,mouseY,80,80)");
 
    gui = createGui();
-   on = createButton("Button", 50, 50);
+   //creando boton
+   on = createButton("Button", 50, 15,150,50);
+
+   //Creando Toggle
+    t = createToggle("Toggle", 200, 15, 350, 50);
+
+    on.setStyle({
+    fillBg: color("#a09e4e"),
+    rounding: 25,
+    textSize: 20,
+  });
 
 
 }
+  
+
 
 function draw() {
-  // put drawing code here
-  background(100,150,30);
+  background("#f00e4f");
   drawGui(); 
+  Stroke();
+  nofill();
+  rect(45,15,windowWidth-45,windowHeight/3);
+  
 
-  /*image(capvid,0,0,height,width);
-  filter(INVERT);
-   */
-  //dibujando un boton
+  //image(capvid,200,95,720,420);
+ 
+   
+   
+    if(on.isPressed) {
+        print( on.label + " is pressed. "+  i++ );
+      }
+  if (on.isHeld) {
+    // Draw an ellipse when Button is he.
+
+    fill(132,206,9);
+    textSize(i);
+    text(" size "+i*10,150,200);
     
-
-    if(b.isPressed) {
-    print(b.label + " is pressed.");
-  }
-  if (b.isHeld) {
-    // Draw an ellipse when Button is held.
-
-    fill(255, 0, 0);
     ellipse(200, 300, 100);
+    
   }
 
   /// Add these lines below sketch to prevent scrolling on mobile
